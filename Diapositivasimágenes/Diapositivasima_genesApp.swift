81e315bad8043d60e7cfa2fdd4ff397e3178cd04
Marco <empleomarcotapia@gmail.com>
2024-04-10 06:19:10 -0700

    Initial Commit

---
+++ Diapositivasimágenes/Diapositivasima_genesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diapositivasima_genesApp.swift
+//  Diapositivasimágenes
+//
+//  Created by Marco on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Diapositivasima_genesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
